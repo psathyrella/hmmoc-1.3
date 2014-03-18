01b10ad8b90ae887a0d37370465effec154a2bfa
Duncan Ralph <dkralph@gmail.com>
2014-03-18 11:08:55 -0700

    Merge in stash from branch modifying aligner
    arg... I screwed this up. Should have been a simple merge, since I'd
    only *really* modified the files in casino/... but I had 'modified' all
    the stuff in aligner/ and other places because in order to get back to
    the two-state casino state I copied from the hmmoc repo. ick. Should be
    ok now but history is fsckd.

--- examples/aligner/main.cc
+++ examples/aligner/main.cc
@@ -7,43 +7,181 @@
 #include <cassert>
 
 //----------------------------------------------------------------------------------------
-class Pars {
+class Pars 
+{
 public:
-  double iSigma;   // substitution rate
-  double iDelta;   // indel probability
-  double iR;       // indel lenght parameter
-  double iTau;     // sequence length parameter
+  double tau; // length parameter
+  double gamma1; // rate G1 to G2
+  double gamma2; // rate G2 to G1
 };
-
 //----------------------------------------------------------------------------------------
-// Accessor classes
-class NoBanding {
+template <class T_dpt, class T_ne_dpt, class T_bwc>
+class HMMDriver
+{
 public:
-  typedef AlignDPTable    DPTable;
-  typedef AlignBaumWelch  BaumWelchCounters;
-  static bfloat Forward(DPTable** ppOutTable,Pars pars,vector<char>& iSequence1,vector<char>& iSequence2) {
-    return ::Forward(ppOutTable,pars.iDelta,pars.iR,pars.iSigma,pars.iTau,iSequence1,iSequence2);
-  }
-  static bfloat Backward(DPTable** ppOutTable,Pars pars,vector<char>& iSequence1,vector<char>& iSequence2) {
-    return ::Backward(ppOutTable,pars.iDelta,pars.iR,pars.iSigma,pars.iTau,iSequence1,iSequence2);
-  }
-  static bfloat BackwardBaumWelch(BaumWelchCounters& bw, DPTable* pInTable,Pars pars,vector<char>& iSequence1,vector<char>& iSequence2) {
-    return ::BackwardBaumWelch(bw, pInTable,pars.iDelta,pars.iR,pars.iSigma,pars.iTau,iSequence1,iSequence2);
-  }
-  static bfloat Viterbi_recurse(DPTable** ppOutTable,Pars pars,vector<char>& iSequence1,vector<char>& iSequence2) {
-    return ::Viterbi_recurse(ppOutTable,pars.iDelta,pars.iR,pars.iSigma,pars.iTau,iSequence1,iSequence2);
-  }
-  static Path& Viterbi_trace(DPTable* pInTable,Pars pars,vector<char>& iSequence1,vector<char>& iSequence2) {
-    return ::Viterbi_trace(pInTable,pars.iDelta,pars.iR,pars.iSigma,pars.iTau,iSequence1,iSequence2);
-  }
-  static Path& Sample(DPTable* pInTable,Pars pars,vector<char>& iSequence1,vector<char>& iSequence2) {
-    return ::Sample(pInTable,pars.iDelta,pars.iR,pars.iSigma,pars.iTau,iSequence1,iSequence2);
-  }
-};
+  HMMDriver(Pars &pars, unsigned n_states);
+  void sample();
+  void estimate();
+  void viterbi();
+  void report();
 
+  unsigned n_states;
+  Pars pars;
+  vector<double> emit_probs_1,emit_probs_2;
+  T_ne_dpt *ne_dptable;                      // DP table for emmissionless HMM
+  T_bwc *bw_counters;                        // Baum Welch counters for regular HMM
+  T_dpt *fw_dptable,*bw_dptable,*vt_dptable; // DP tables for regular HMM
+  map<unsigned,string> ne_state_ids;         // state ID translators for emmissionless and regular HMM
+  map<unsigned,string> state_ids;            // (no, they're not necessarily the *#$! same)
+  vector<char> sampled_seq;                  // true (sampled) sequence
+  Path *true_path,*path_out;                 // true (sampled) path out viterbi path
+  unsigned iterations;
+};
+//----------------------------------------------------------------------------------------
+template <class T_dpt, class T_ne_dpt, class T_bwc>
+HMMDriver<T_dpt, T_ne_dpt, T_bwc>::HMMDriver(Pars &pars, unsigned n_states):
+  n_states(n_states),
+  pars(pars),
+  emit_probs_1({0.1,0.1,0.4,0.4}),
+  emit_probs_2({0.1,0.4,0.1,0.4}),
+  ne_dptable(0),
+  bw_counters(0),
+  fw_dptable(0),
+  bw_dptable(0),
+  vt_dptable(0),
+  true_path(0),
+  path_out(0),
+  iterations(2)
+{
+}
+//----------------------------------------------------------------------------------------
+template <class T_dpt, class T_ne_dpt, class T_bwc>
+void HMMDriver<T_dpt, T_ne_dpt, T_bwc>::sample() 
+{
+  cout << "sampling " << endl;
+  do {
+    NEBackward(&ne_dptable, pars.tau, pars.gamma1, pars.gamma2);           // Initialize the DP table using the Backward algorithm
+    true_path = &NESample(ne_dptable, pars.tau, pars.gamma1, pars.gamma2); // Sample
+  } while ((true_path->size() - 1) < 1/pars.tau);            // We don't want short sequences
+  cout << setw(25) << (true_path->size() - 1) << " throws" << endl;
+  for(unsigned is=0; is<n_states; is++) ne_state_ids[is] = ne_dptable->getStateId(is);
+  const char *nukes = "ACGT"; // surely this is defined somewhere else? can't find it
+  for(unsigned is=0; is<true_path->size(); is++) {
+    assert(ne_state_ids.find(true_path->toState(is)) != ne_state_ids.end());
+    string stateStr = ne_state_ids[true_path->toState(is)];
+    vector<double> *emit_probs(0);
+    if(stateStr=="NEgenerate1")      emit_probs = &emit_probs_1;
+    else if(stateStr=="NEgenerate2") emit_probs = &emit_probs_2;
+    else if(stateStr=="NEbegin" || stateStr=="NEstop") {
+      break;
+    } else {
+      cout << "ACK: " << stateStr << endl;
+      assert(0);
+    }
+      
+    double prob = random() / (double)RAND_MAX;
+    unsigned ip(0);
+    while(true) {
+      if(prob < (*emit_probs)[ip]) {
+	sampled_seq.push_back(nukes[ip]);
+	break;
+      }
+      prob -= (*emit_probs)[ip];
+      ip++;
+      assert(prob>0);
+    }
+  }
+}
+//----------------------------------------------------------------------------------------
+// use Baum-Welch training to estimate parameters
+template <class T_dpt, class T_ne_dpt, class T_bwc>
+void HMMDriver<T_dpt, T_ne_dpt, T_bwc>::estimate()
+{
+  bw_counters = new T_bwc;
+  cout << "parameter estimation:" << endl;
+  cout << setw(12) << "iteration" << setw(12) << "likelihood"
+       << setw(12) << "gamma1"
+       << setw(12) << "gamma2"
+       << endl;
+  for(unsigned iter=1; iter<=iterations; ++iter) {
+    bfloat fw = Forward(&fw_dptable, pars.tau, pars.gamma1, pars.gamma2, sampled_seq, emit_probs_1, emit_probs_2); // calculate the forward DP table
+    // calculate the Baum-Welch estimated transition counts
+    bw_counters->resetCounts();
+    Backward(*bw_counters, fw_dptable, pars.tau, pars.gamma1, pars.gamma2, sampled_seq, emit_probs_1, emit_probs_2);
+    cout
+      << setw(12) << iter
+      << setw(12) << fw
+      << setw(12) << pars.gamma1
+      << setw(12) << pars.gamma2
+      << endl;
+    delete fw_dptable;
+    fw_dptable = 0;
+    // get the expected transition counts
+
+    cout
+      // << setw(12) << "trBG1"  << setw(12) << bw_counters->transitionIndex("trBG1" ) << setw(12) << bw_counters->transitionBaumWelchCount0[bw_counters->transitionIndex("trBG1" )] << endl
+      // << setw(12) << "trBG2"  << setw(12) << bw_counters->transitionIndex("trBG2" ) << setw(12) << bw_counters->transitionBaumWelchCount0[bw_counters->transitionIndex("trBG2" )] << endl
+      // << setw(12) << "trG1G1" << setw(12) << bw_counters->transitionIndex("trG1G1") << setw(12) << bw_counters->transitionBaumWelchCount0[bw_counters->transitionIndex("trG1G1")] << endl
+      // << setw(12) << "trG1G2" << setw(12) << bw_counters->transitionIndex("trG1G2") << setw(12) << bw_counters->transitionBaumWelchCount0[bw_counters->transitionIndex("trG1G2")] << endl
+      // << setw(12) << "trG2G2" << setw(12) << bw_counters->transitionIndex("trG2G2") << setw(12) << bw_counters->transitionBaumWelchCount0[bw_counters->transitionIndex("trG2G2")] << endl
+      // << setw(12) << "trG2G1" << setw(12) << bw_counters->transitionIndex("trG2G1") << setw(12) << bw_counters->transitionBaumWelchCount0[bw_counters->transitionIndex("trG2G1")] << endl
+      // << setw(12) << "trG1S"  << setw(12) << bw_counters->transitionIndex("trG1S" ) << setw(12) << bw_counters->transitionBaumWelchCount0[bw_counters->transitionIndex("trG1S" )] << endl
+      // << setw(12) << "trG2S"  << setw(12) << bw_counters->transitionIndex("trG2S" ) << setw(12) << bw_counters->transitionBaumWelchCount0[bw_counters->transitionIndex("trG2S" )] << endl;
+      << setw(12) << "emit1"  << setw(12) << bw_counters->emissionIndex("emit1" ) << setw(12) << bw_counters->emissionBaumWelchCount0[bw_counters->emissionIndex("emit1" )] << endl
+      << setw(12) << "emit2"  << setw(12) << bw_counters->emissionIndex("emit2" ) << setw(12) << bw_counters->emissionBaumWelchCount0[bw_counters->emissionIndex("emit2" )] << endl
+      << setw(12) << "empty"  << setw(12) << bw_counters->emissionIndex("empty" ) << setw(12) << bw_counters->emissionBaumWelchCount0[bw_counters->emissionIndex("empty" )] << endl;
+
+    // calculate the new parameters
+    // double trG1G2 = bw_counters->transitionBaumWelchCount0[ bw_counters->transitionIndex("trG1G2") ];
+    // double trG2G1 = bw_counters->transitionBaumWelchCount0[ bw_counters->transitionIndex("trG2G1") ];
+    // pars.gamma1 = trG1G2 + 0.0001;
+    // pars.gamma2 = trG2G1 + 0.0001;
+  }
+  delete bw_counters;
+  bw_counters = 0;
+}
+//----------------------------------------------------------------------------------------
+// Compute a Viterbi alignment
+template <class T_dpt, class T_ne_dpt, class T_bwc>
+void HMMDriver<T_dpt, T_ne_dpt, T_bwc>::viterbi()
+{
+  cout << "viterbi recursion and traceback" << endl;
+  Viterbi_recurse(&vt_dptable, pars.tau, pars.gamma1, pars.gamma2, sampled_seq, emit_probs_1, emit_probs_2);
+  path_out = &Viterbi_trace(vt_dptable, pars.tau, pars.gamma1, pars.gamma2, sampled_seq, emit_probs_1, emit_probs_2);
+  for(unsigned is=0; is<n_states; is++) state_ids[is] = vt_dptable->getStateId(is);
+  delete vt_dptable;
+  vt_dptable = 0;
+}
+//----------------------------------------------------------------------------------------
+// Print the Viterbi alignment, and a summary of the posteriors
+template <class T_dpt, class T_ne_dpt, class T_bwc>
+void HMMDriver<T_dpt, T_ne_dpt, T_bwc>::report() 
+{
+  string seqStr(""),pathStr("");
+  unsigned imax = max((unsigned)path_out->size(), (unsigned)sampled_seq.size());
+  for(unsigned is=0; is<imax; is++) {
+    if(is < path_out->size()) {
+      assert(state_ids.find(path_out->toState(is)) != state_ids.end());
+      pathStr += *(state_ids[path_out->toState(is)].end()-1);
+    }
+    if(is < sampled_seq.size()) {
+      seqStr += sampled_seq[is];
+    }
+  }
+  cout << "emitted sequence " << seqStr << endl;
+  cout << "vtb path         " << pathStr << endl;
+
+  string true_path_str("");
+  for(unsigned is=0; is<true_path->size(); is++) {
+    assert(ne_state_ids.find(true_path->toState(is)) != ne_state_ids.end());
+    true_path_str += *(ne_state_ids[true_path->toState(is)].end()-1);
+  }
+  cout << "true path        " << true_path_str << endl;
+}
 //----------------------------------------------------------------------------------------
 // read input file: first column should be sequence IDs, second column the sequences. Gaps are removed and sequences are uppercased
-void readFile(istream& is, vector<pair<string,vector<char> > > &seqs) {
+void readFile(istream& is, vector<pair<string,vector<char> > > &seqs) 
+{
   string line;
   while(getline(is,line)) {
     stringstream ss(line);
@@ -73,203 +211,18 @@ void readFile(istream& is, vector<pair<string,vector<char> > > &seqs) {
     seqs.push_back(newSequence);
   }
 }
-
 //----------------------------------------------------------------------------------------
-// use Baum-Welch training to estimate transition (i.e., indel) parameters
-template<class T>
-void estimate( int iterations, Pars& params, vector<char>& iSeq1, vector<char>& iSeq2 ) {
-  typename T::DPTable* pFW;
-  typename T::BaumWelchCounters baumWelch;
-  for (int iter=1; iter <= iterations; ++iter) {
-    // calculate the forward DP table
-    cout << "Forward..." << endl;
-    bfloat fw = T::Forward(&pFW, params, iSeq1, iSeq2);
-
-    // calculate the Baum-Welch estimated transition counts
-    baumWelch.resetCounts();
-    cout << "Baum-Welch + backward..." << endl;
-    bfloat bw = T::BackwardBaumWelch(baumWelch, pFW, params, iSeq1, iSeq2);
-
-    cout << "Iteration " << iter << ": likelihood = " << fw << endl;
-    cout << "estimated params: "
-	 << setw(12) << params.iSigma
-	 << setw(12) << params.iDelta
-	 << setw(12) << params.iR
-	 << setw(12) << params.iTau
-	 << endl;
-
-    // remove the forward table
-    delete pFW;
-
-    // get the expected transition counts
-    double trMM = baumWelch.transitionBaumWelchCount00[ baumWelch.transitionIndex("trMM") ];
-    double trMI = baumWelch.transitionBaumWelchCount00[ baumWelch.transitionIndex("trMI") ];
-    double trMD = baumWelch.transitionBaumWelchCount00[ baumWelch.transitionIndex("trMD") ];
-
-    double trII = baumWelch.transitionBaumWelchCount00[ baumWelch.transitionIndex("trII") ];
-    double trIM = baumWelch.transitionBaumWelchCount00[ baumWelch.transitionIndex("trIM") ];
-    double trDD = baumWelch.transitionBaumWelchCount00[ baumWelch.transitionIndex("trDD") ];
-    double trDM = baumWelch.transitionBaumWelchCount00[ baumWelch.transitionIndex("trDM") ];
-
-    // calculate the new parameters
-    params.iR = (trII + trDD) / (trII + trIM + trDD + trDM);
-    params.iDelta = (trMI + trMD) / (trMI + trMD + trMM);
-  }
-}
-    
-//----------------------------------------------------------------------------------------
-// Compute a Viterbi alignment
-template<class T>
-Path& viterbi( Pars& pars, vector<char> iSeq1, vector<char> iSeq2 ) {
-  typename T::DPTable* pViterbiTable;
-  cout << "Viterbi recursion..." << endl;
-  T::Viterbi_recurse(&pViterbiTable, pars, iSeq1, iSeq2 );
-  cout << "Viterbi traceback..." << endl;
-  Path& path = T::Viterbi_trace(pViterbiTable, pars, iSeq1, iSeq2 );
-  delete pViterbiTable;
-  return path;
-}
-
-//----------------------------------------------------------------------------------------
-// This computes the posterior probability of being in a particular state
-template<class T>
-double posterior( const string state, int i, int j, typename T::DPTable* pFW, typename T::DPTable* pBW ) {
-  if (pFW->outputId[0] == "sequence2") {
-    // the first coordinate in getProb refers to the fastest coordinate (inner loop); in this
-    // case, this is sequence2
-    swap(i,j);
-  }
-  bfloat total = pBW->getProb("start",0,0);
-  bfloat fw = pFW->getProb( state, i, j );
-  bfloat bw = pBW->getProb( state, i, j );
-  return (fw * bw)/total;
-}
-
-//----------------------------------------------------------------------------------------
-// Compute posteriors for alignment columns, marginalized over gap positions
-//
-template<class T>
-double marginalizedPosterior( const string state, int i, int j, typename T::DPTable* pFW, typename T::DPTable* pBW ) {
-  if (state == "match") {
-    return posterior<T>( state, i, j, pFW, pBW );
-  } else if (state == "insert") {
-    double p = 0.0;
-    for (int y=0; y < pFW->iLen2; ++y) {
-      p += posterior<T>( state, i, y, pFW, pBW );
-    }
-    return p;
-  } else if (state == "delete") {
-    double p = 0.0;
-    for (int x=0; x < pFW->iLen1; ++x) {
-      p += posterior<T>( state, x, j, pFW, pBW );
-    }
-    return p;
-  }
-  cout << "Unknown state: " << state << endl;
-  return 0;
-}
-
-//----------------------------------------------------------------------------------------
-// Print the Viterbi alignment, and a summary of the posteriors
-//
-template<class T>
-void report( Path& path, typename T::DPTable* pFW, typename T::DPTable* pBW, vector<char>& iSeq1, vector<char>& iSeq2 ) {
-  int x = 0;
-  int y = 0;
-  string line1 = "";
-  string line2 = "";
-  string line3 = "";
-  // Loop over all transitions, except the last one (to the end state)
-  double pTot(0); // total probability
-  for (int i=0; i < (int)path.size()-1; i++) {
-    int stateId = path.toState(i);     
-    string state = pFW->stateId[stateId];
-    if (state == "match") {
-      line1 += iSeq1[x++];
-      line2 += iSeq2[y++];
-    } else if (state == "insert") {
-      line1 += iSeq1[x++];
-      line2 += "-";
-    } else if (state == "delete") {
-      line1 += "-";
-      line2 += iSeq2[y++];
-    }
-    double posterior = marginalizedPosterior<T>( state, x, y, pFW, pBW );
-    pTot += posterior;
-    char postId = "0123456789"[ min( int(posterior*10.0), 9) ];
-    line3 += postId;
-  }
-  cout << line1 << endl;
-  cout << line2 << endl;
-  cout << line3 << endl;
-  // ofstream ofs("out.txt",ios_base::app);
-  // ofs << setw(12) << pTot;
-  // ofs.close();
-}
-
-template<class T>
-void execute( int iterations, Pars pars, vector<char>& iSeq1, vector<char>& iSeq2 ) {
-    // estimate iDelta and iR using Baum-Welch training
-    estimate<T>( iterations, pars, iSeq1, iSeq2 );
-    cout << "estimated params: "
-	 << setw(12) << pars.iSigma
-	 << setw(12) << pars.iDelta
-	 << setw(12) << pars.iR
-	 << setw(12) << pars.iTau
-	 << endl;
-
-    // calculate the Viterbi path
-    Path& path = viterbi<T>(pars, iSeq1, iSeq2 );
-
-    // calculate the Forward and Backward tables, to compute posteriors
-    typename T::DPTable *pFW, *pBW;
-    cout << "Forward..." << endl;
-    T::Forward (&pFW, pars, iSeq1, iSeq2 );
-    cout << "Backward..." << endl;
-    T::Backward (&pBW, pars, iSeq1, iSeq2 );
-
-    // report the results
-    cout << endl << "Alignment:" << endl;
-    report<T>( path, pFW, pBW, iSeq1, iSeq2 );
-
-    delete pFW;
-    delete pBW;
-}
-
-int main(int argc, char** argv) {
-  // set initial parameters
+int main(int argc, char** argv) 
+{
+  srand(getpid());
   Pars pars;
-  pars.iSigma = 0.4;
-  pars.iDelta = 0.05;
-  pars.iR = 0.6;
-  pars.iTau = 0.0001;
-
-  char* file = (char*)"sequence.fa";
-  if (argc == 1) {
-    cout << "Usage: " << argv[0] << " file.fa \n" << endl;
-    cout << "'file.fa':   two nucleotide sequences on lines 2 and 3; gaps characters are ignored.\n";
-    cout << "Using default: file=sequence.fa:\n" << endl;
-  } else {
-    file = argv[1];
-  }
-  if (argc > 2) {
-    cout << "Too many parameters" << endl;
-    exit(1);
-  }
-
-  std::ifstream ifsIn( file );
-  vector<pair<string,vector<char> > > seqs;
-  readFile(ifsIn, seqs);
-  for(unsigned i1=0; i1<seqs.size(); i1++) {
-    vector<char>& iSeq1 = seqs[i1].second;
-    for(unsigned i2=i1+1; i2<seqs.size(); i2++) {
-      vector<char>& iSeq2 = seqs[i2].second;
-      cout << "Read sequences.  Lengths: " << iSeq1.size() << " and " << iSeq2.size() << ".  ";
-      execute<NoBanding>( 5, pars, iSeq1, iSeq2 );
-      assert (0);
-    }
-    // ofstream ofs("out.txt",ios_base::app);
-    // ofs << endl;
-    // ofs.close();
-  }
+  pars.tau = 0.1;
+  pars.gamma1 = 0.5;
+  pars.gamma2 = 0.5;
+  unsigned n_states(4);
+  HMMDriver<SeqGenDPTable, NESeqGenDPTable, SeqGenBaumWelch> hmmd(pars, n_states);
+  hmmd.sample();
+  hmmd.estimate();
+  hmmd.viterbi();
+  hmmd.report();
 }
--- examples/casino/casinomain.cc
+++ examples/casino/casinomain.cc
@@ -3,6 +3,15 @@
 #include <iomanip>
 #include <cassert>
 
+//----------------------------------------------------------------------------------------
+// make sure probabilities sum to 1
+void check_probabilities(const vector<double> probs) {
+  double total(0.0);
+  for (unsigned ip=0; ip<probs.size(); ip++)
+    total += probs[ip];
+  assert(total == 1.0);
+}
+//----------------------------------------------------------------------------------------
 int main(void) {
   srand(getpid());
 
@@ -10,18 +19,16 @@ int main(void) {
   Pars pars;
   pars.iGoHonest = 0.02;
   pars.iGoDishonest = 0.05;
-  pars.iGoStop = 0.005;
-  // pars.aEmitHonest = {0.25, 0.25, 0.25, 0.25};
-  // pars.aEmitDishonest = {0.1, 0.1, 0.1, 0.7};
-  pars.aEmitHonest = {0.1, 0.4, 0.1, 0.4};
-  pars.aEmitDishonest = {0.1, 0.1, 0.4, 0.4};
+  pars.iGoStop = 0.01;
+  pars.aEmitHonest = {0.1, 0.1, 0.4, 0.4};
+  pars.aEmitDishonest = {0.1, 0.4, 0.1, 0.4};
   assert(n_letters == pars.aEmitHonest.size());
   assert(n_letters == pars.aEmitDishonest.size());
+  check_probabilities(pars.aEmitHonest);
+  check_probabilities(pars.aEmitDishonest);
 
-  //
   // Sample a path.  This uses the HMM that does not emit symbols, because we want to sample from the HMM
   // itself, not conditional on an output sequence.
-  //
   int iPathLength;
   Path *pTruePath;
   NoEmissionCasinoDPTable* pDPNE;               
@@ -32,9 +39,7 @@ int main(void) {
   } while (iPathLength < 1/pars.iGoStop);         // We don't want short sequences
   cout << "Sampled " << iPathLength << " throws" << endl;
 
-  //
   // Next, build an input emission sequence by sampling the emitted symbols according to true path
-  //
   char* aSequence = new char[ iPathLength ];
   for (int i=0; i<iPathLength; i++) {
     double iP = random() / (double)RAND_MAX;
@@ -56,24 +61,31 @@ int main(void) {
   }
 
   // Decode the emission sequence using Viterbi, and compute posteriors and Baum Welch counts using Forward and Backward
-  CasinoDPTable *pViterbiDP, *pFWDP, *pBWDP;
+  CasinoDPTable *pFWDP(0), *pBWDP(0);
   CasinoBaumWelch bw;
-
-  cout << "Calculating Viterbi probability..." << endl;
-  bfloat iVitProb = Viterbi_recurse(&pViterbiDP, pars, aSequence, iPathLength );
-  cout << "Viterbi: "<<iVitProb<<endl;
-
-  cout << "Calculating Forward probability..." << endl;
+  cout << setw(22) << "forward probability:";
   bfloat iFWProb = Forward(&pFWDP, pars, aSequence, iPathLength );
-  cout << "Forward: "<<iFWProb<<endl;
-
-  cout << "Calculating Backward probability..." << endl;
+  cout << setw(12) << iFWProb << endl;
+  cout << setw(22) << "backward probability: ";
   bfloat iBWProb = Backward(bw, pFWDP, &pBWDP, pars, aSequence, iPathLength );
-  cout << "Backward:"<<iBWProb<<endl;
-  
-  cout << "Calculating Viterbi path..." << endl;
+  cout << setw(12) << iBWProb << endl;
+
+  cout << "Baum Welch emission counts:"<<endl;
+  cout << setw(12) << " " << setw(12) << "Honest" << setw(12) << "Dishonest" << endl;
+  for (unsigned i=0; i<n_letters; i++) {
+    cout << setw(12) << i
+// ???	 << setw(12) << "emitHonest"  << setw(12) << bw_counters->emissionIndex("emitHonest" ) << setw(12) << bw_counters->emissionBaumWelchCount0[bw_counters->emissionIndex("emitHonest" )] << endl
+	 << setw(12) << bw.emissionBaumWelchCount1[i][ bw.emissionIndex("emitHonest") ]
+	 << setw(12) << bw.emissionBaumWelchCount1[i][ bw.emissionIndex("emitDishonest") ]
+	 << endl;
+  }
+
+  CasinoDPTable *pViterbiDP(0);
+  cout << "viterbi probability: ";
+  bfloat iVitProb = Viterbi_recurse(&pViterbiDP, pars, aSequence, iPathLength );
+  cout << setw(12) << iVitProb << endl;
   Path& iViterbiPath = Viterbi_trace(pViterbiDP, pars, aSequence, iPathLength );
-  
+
   // Find out how many states the Viterbi algorithm recovered correctly
   int iNewCorrect = 0;
   for (int i=0; i<(int)iViterbiPath.size(); i++) {      // include last transition to the 'end' state
@@ -88,32 +100,27 @@ int main(void) {
        << setw(12) << iNewCorrect << " / " << iViterbiPath.size()
        << " = " << (double(iNewCorrect) / iViterbiPath.size()) << endl;
 
-  cout << "Baum Welch emission counts:"<<endl;
-  cout << setw(12) << " " << setw(12) << "Honest" << setw(12) << "Dishonest" << endl;
-  for (unsigned i=0; i<n_letters; i++) {
-    cout << setw(12) << i
-	 << setw(12) << bw.emissionBaumWelchCount1[i][ bw.emissionIndex("emitHonest") ]
-	 << setw(12) << bw.emissionBaumWelchCount1[i][ bw.emissionIndex("emitDishonest") ]
-	 << endl;
-  }
-
   // Compare the true and Viterbi paths, and print the posterior
   // probability of being in the honest state
-  for (int i=0; i<min(iPathLength,30); i++) {
-    cout << aSequence[i] << " True:";
+  string sampled_seq,vtb_path,true_path;
+  for (int i=0; i<iPathLength; i++) {
+    sampled_seq += aSequence[i];
     if (pDPNE->getStateId(pTruePath->toState(i)) == "NEhonest") {
-      cout << "H";
+      true_path += "h";
     } else if (pDPNE->getStateId(pTruePath->toState(i)) == "NEdishonest") {
-      cout << "D";
-    } else assert(0);
-    cout << " Viterbi:";
+      true_path += "d";
+    } else {
+      assert(0);
+    }
     if (pViterbiDP->getStateId(iViterbiPath.toState(i)) == "honest") {
-      cout << "H";
+      vtb_path += "h";
     } else if (pViterbiDP->getStateId(iViterbiPath.toState(i)) == "dishonest") {
-      cout << "D";
+      vtb_path += "d";
     } else assert(0);
-
-    double iPosterior = pFWDP->getProb("honest",i+1)*pBWDP->getProb("honest",i+1)/iFWProb;
-    cout << " " << iPosterior << endl;
+    // double iPosterior = pFWDP->getProb("honest",i+1)*pBWDP->getProb("honest",i+1)/iFWProb;
+    // cout << " " << iPosterior << endl;
   }
+  cout << setw(22) << "emitted sequence: " << sampled_seq << endl;
+  cout << setw(22) << "vtb path: " << vtb_path << endl;
+  cout << setw(22) << "true path: " << true_path << endl;
 }
